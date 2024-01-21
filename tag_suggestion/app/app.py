d146cc021eadc6bb7841972381104574666dfe7f
Fouad Mhz <73448082+Fouad-Mhz@users.noreply.github.com>
2024-01-21 07:07:05 +0100

    Update app.py

--- tag_suggestion/app/app.py
+++ tag_suggestion/app/app.py
@@ -1,19 +1,19 @@
 import streamlit as st
-from pages import monitor_page, meta_page, inference_page
+from pages import inference_page, base_models_page, meta_model_page
 
 def main():
     st.sidebar.title("Navigation")
 
     # Create a sidebar with page options
-    page_options = ["Monitor Page", "Meta Page", "Inference Page"]
+    page_options = ["Base models monitoring", "Meta model monitoring", "Inference"]
     selected_page = st.sidebar.radio("Select Page", page_options)
 
     # Render the selected page
-    if selected_page == "Monitor Page":
-        monitor_page.show()
-    elif selected_page == "Meta Page":
-        meta_page.show()
-    elif selected_page == "Inference Page":
+    if selected_page == "Base models monitoring":
+        base_models_page.show()
+    elif selected_page == "Meta model monitoring":
+        meta_model_page.show()
+    elif selected_page == "Inference":
         inference_page.show()
 
 if __name__ == "__main__":
